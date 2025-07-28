f3d4632584b3f6a7aeb90a5bcbe549dfc15ddf61
pranavmudholkar <pranavmudholkar455@gmail.com>
2025-07-28 17:57:20 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// const heading = React.createElement(
+// 	'h1',
+// 	{ id: 'heading' },
+// 	'Hello World from React!'
+// );
+
+const parent = React.createElement(
+	'div',
+	{ id: 'parent' },
+	React.createElement('div', { id: 'child' }, [
+		React.createElement('h1', {}, "I'm an H1 tag!!"),
+		React.createElement('h2', {}, "I'm an H2 tag!!"),
+	])
+);
+
+console.log(parent); //return an object
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
