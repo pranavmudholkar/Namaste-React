53b7f02fc95901d5768ca1ae8515bd7147666779
pranavmudholkar <pranavmudholkar455@gmail.com>
2025-07-30 10:54:59 +0530

    configuring the react app

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-// const heading = React.createElement(
-// 	'h1',
-// 	{ id: 'heading' },
-// 	'Hello World from React!'
-// );
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 const parent = React.createElement(
 	'div',
 	{ id: 'parent' },
+	React.createElement('div', { id: 'child' }, [
+		React.createElement('h1', {}, "I'm an H1 tag!!"),
+		React.createElement('h2', {}, "I'm an H2 tag!!"),
+	]),
 	React.createElement('div', { id: 'child' }, [
 		React.createElement('h1', {}, "I'm an H1 tag!!"),
 		React.createElement('h2', {}, "I'm an H2 tag!!"),
