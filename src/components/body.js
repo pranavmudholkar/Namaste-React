4790b1228283a6f0fadc3d24077dbf45ff69229f
pranavmudholkar <pranavmudholkar455@gmail.com>
2025-08-04 22:18:17 +0530

    filter functionality

--- src/components/body.js
+++ src/components/body.js
@@ -5,33 +5,59 @@ import Shimmer from './shimmer';
 const Body = () => {
 	const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
+	const [searchText, setSearchText] = useState('');
+
 	useEffect(() => {
+		const fetchData = async () => {
+			const data = await fetch(
+				'https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.38430&lng=78.45830&is-seo-homepage-enabled=true&page_type=DESKTOP_'
+			);
+			const json = await data.json();
+			setListOfRestaurants(
+				json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+					?.restaurants
+			);
+			// console.log(
+			// 	json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+			// 		?.restaurants
+			// );
+		};
 		fetchData();
 	}, []);
-
-	const fetchData = async () => {
-		const data = await fetch(
-			'https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.38430&lng=78.45830&is-seo-homepage-enabled=true&page_type=DESKTOP_'
-		);
-		const json = await data.json();
-		setListOfRestaurants(
-			json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-		);
-		console.log(json.data);
-	};
 
 	return listOfRestaurants.length === 0 ? (
 		<Shimmer />
 	) : (
 		<div className='body'>
 			<div className='filter'>
+				<div className='search'>
+					<input
+						type='text'
+						className='search-box'
+						value={searchText}
+						onChange={(e) => {
+							setSearchText(e.target.value);
+						}}
+					/>
+					<button
+						onClick={() => {
+							console.log(searchText);
+							const filteredRestaurant = listOfRestaurants.filter((res) =>
+								res.info.name.toLowerCase().includes(searchText.toLowerCase())
+							);
+							setListOfRestaurants(filteredRestaurant);
+						}}
+					>
+						Search
+					</button>
+				</div>
 				<button
 					className='filter-btn'
 					onClick={() => {
 						const filteredList = listOfRestaurants.filter(
 							(res) => res.info.avgRating > 4
 						);
-						console.log(filteredList);
+						// console.log(filteredList);
 						setListOfRestaurants(filteredList);
 					}}
 				>
